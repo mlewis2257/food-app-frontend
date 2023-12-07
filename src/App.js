368f64ce3ea661f172267d68f6bdc79791880f81
mlewis2257 <mlang.lewis@gmail.com>
2023-12-07 14:06:52 -0800

    app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([{}]);
+  useEffect(function () {
+    async function getData() {
+      try {
+        const res = await axios.get("http://127.0.0.1:8000/api/");
+        setData(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getData();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Food List</h1>
+        <h3>{data[0].title}</h3>
+        <h3>{data[0].description}</h3>
+        <h3>{data[0].completed}</h3>
+      </div>
     </div>
   );
 }
